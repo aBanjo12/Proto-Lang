b1d790be433b42e93e997cddd311838f8cd0022b
aBanjo111 <landonlmk@gmail.com>
2023-09-05 20:05:59 -0400

    add files

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/lando/Documents/GitHub/Proto-Lang
+# Build directory: C:/Users/lando/Documents/GitHub/Proto-Lang/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
