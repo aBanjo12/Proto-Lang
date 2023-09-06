be6b187feea9c3a6d2fb6ffcd35454a2a5fbf79b
aBanjo111 <76058655+aBanjo111@users.noreply.github.com>
2023-09-06 16:35:40 +0000

    Start writing lexer

--- main.c
+++ main.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
 
-int main()
+int main(int argc, char* argv[])
 {
     printf("Hello, from proto-lang!\n");
+    for (int i = 1; i < argc; i++)
+    {
+        printf("%s\n", argv[i]);
+    }
+
 }
+
+void Lex(char* arg1[])
+{
+    typedef enum TokenType {
+        INT = 0, PLUSSIGN = 1
+    } TokenType;
+
+    typedef struct Token {
+        enum TokenType type;
+        char text[];
+    } Token;
+
+    Token tokens[sizeof(arg1) - sizeof(arg1[0])];
+
+    for (int i = 0; i < sizeof(arg1) - sizeof(arg1[0]); i++)
+    {
+        switch (*arg1[i])
+        {
+            case 0:
+                //tokens[i] = (Token){.type = INT, .text = IDK HOW TO DO THIS};
+        }
+    }
+
+}
+
