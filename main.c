855e898ae641374ae9e521034c05e764f90dfb37
aBanjo111 <76058655+aBanjo111@users.noreply.github.com>
2023-09-07 18:27:27 +0000

    update gitignore, delete build files, strtok stuff

--- main.c
+++ main.c
@@ -1,16 +1,7 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(int argc, char* argv[])
-{
-    printf("Hello, from proto-lang!\n");
-    for (int i = 1; i < argc; i++)
-    {
-        printf("%s\n", argv[i]);
-    }
-
-}
-
-void Lex(char* arg1[])
+void Lex()
 {
     typedef enum TokenType {
         INT = 0, PLUSSIGN = 1
@@ -21,7 +12,21 @@ void Lex(char* arg1[])
         char text[];
     } Token;
 
-    Token tokens[sizeof(arg1) - sizeof(arg1[0])];
+    char s[60] = "this is a test sentance because brock is a silly mate X_X";
+    char t[2] = " ";
+    char tokens[40];
+
+    char* e = strtok(s, t);
+
+    for (int i = 0; e != NULL; i++)
+    {
+        printf("%s\n", e);
+        tokens[i] = e;
+        e = strtok(NULL, t);
+    }
+
+
+    /*Token tokens[sizeof(arg1) - sizeof(arg1[0])];
 
     for (int i = 0; i < sizeof(arg1) - sizeof(arg1[0]); i++)
     {
@@ -30,7 +35,21 @@ void Lex(char* arg1[])
             case 0:
                 //tokens[i] = (Token){.type = INT, .text = IDK HOW TO DO THIS};
         }
-    }
+    }*/
 
 }
+
+int main(int argc, char* argv[])
+{
+    printf("Hello, from proto-lang!\n");
+    for (int i = 1; i < argc; i++)
+    {
+        printf("%s\n", argv[i]);
+    }
+
+    Lex();
+
+}
+
+
 
